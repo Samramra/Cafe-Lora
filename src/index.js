94225e9fe99af3e944afb9ec009f377a0df2f686
Šárka Mráčková <sarka7091@gmail.com>
2022-04-30 12:04:23 +0200

    js v menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './style.css';
 
 console.log('funguju!');
+const navBtn = document.querySelector('#nav-btn'); /*připojení tlačítka*/
+
+const zmena = () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+};
+navBtn.addEventListener('click', zmena);
+/*změna na klik otevře a na klik zaveře*/
+
+let odkazyNavigace =
+  document.querySelectorAll(
+    'nav > a',
+  ); /*připojení na jednotlivé položky jako doma menu..*/
+for (let i = 0; i < odkazyNavigace.length; i++) {
+  odkazyNavigace[i].addEventListener('click', zmena);
+}
+/*for cyklus mi při kliknutí na položku menu, sroluje na příslušnou část stránky a zavře menu*/
